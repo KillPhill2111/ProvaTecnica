ddcfb3dbf13125bbf890768c5fbf51bbb7c5ddb3
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-08-31 15:44:07 -0300

    resoluçãodo exercicio 01

---
+++ 01/Questão 01- Fibonacci/Questão 01- Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("bem vindo, entre com o numero para saber a sequencia fibonacci: ");
+int numeroSeq=int.Parse(Console.ReadLine());
+
+Console.WriteLine($"A sequência de Fibonacci até o {numeroSeq}º termo é:");
+
+for (int i = 0; i < numeroSeq; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
+
+static int Fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    
+    int a = 0, b = 1, c = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
